d0ef853265e21c51270330f3926018b04dea1337
Qbell <Qbell@aws_workspace>
2023-02-25 22:51:57 -0600

    notes in code

--- create_vector/create_vector.cc
+++ create_vector/create_vector.cc
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-int main ()
+ int main ()
 {
-  // vector<int> v{10,5};
-  // vector<int> v = {10,5};
-  // vector<int> v(10,5);
-  // vector<int> v = (10,5);
+   // vector<int> v{10,5};     // Displays '10 5' because a vector is cretaed
+   // vector<int> v = {10,5};      // Displays '10 5' because a vector is cretaed
+  //  vector<int> v(10,5);     // Displays '5', 10 times. 10 is the maximum # of values while 5 is the actual item.
+   vector<int> v = (10,5);      // Does not return because of the comma operator.
 
   for (auto& elem : v) {
     cout << elem << " ";
