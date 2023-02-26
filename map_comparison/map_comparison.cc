37a86f1701f6f3a0a42d777e727246ccc2b2ee02
Qbell <Qbell@aws_workspace>
2023-02-25 22:54:10 -0600

    notes in code

--- map_comparison/map_comparison.cc
+++ map_comparison/map_comparison.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <map>
 
 /////////////////////////////////////////
 // INCLUDE NECESSARY HEADER FILES HERE //
@@ -14,19 +16,25 @@ void func_map (vector<pair<int,string>>& items)
   ////////////////////////////////////////////////
   // CREATE AN EMPTY map MAPPING int TO string. //
   ////////////////////////////////////////////////
+map<int,string> map;
 
 
   ////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,     //
   // INSERT EACH ITEM IN items INTO THE MAP //
   ////////////////////////////////////////////
-
+pair<int,string> p;     // creates pairs
+for (auto p : items){   // Pairs the keys and values together in "items" 
+ map.insert(p);         // Puts the pairs into the map
+}
 
   //////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,       //
   // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP //
   //////////////////////////////////////////////
-
+for (auto p: map){                                  // iterates the pairs into the map
+    cout << p.first << " " << p.second << "\n";     // Displays the pairs
+}
 
   cout << endl << "=== END func_map ===" << endl;
 }
@@ -38,7 +46,7 @@ void func_unordered_map (vector<pair<int,string>>& items)
   //////////////////////////////////////////////////////////
   // CREATE AN EMPTY unordered_map MAPPING int TO string. //
   //////////////////////////////////////////////////////////
-
+unordered_map<int,string>map;
 
   /////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,      //
@@ -50,13 +58,20 @@ void func_unordered_map (vector<pair<int,string>>& items)
   // IN THE FOLLOWING FORMAT:                //
   //   o  "[N,B,LF] = [3,10,0.4432]"         //
   /////////////////////////////////////////////
+     
+for (auto p: items){   // Pairs the keys and values together in "items" 
+ map.insert(p);         // Puts the pairs into the unordered map
+ cout << "[N=" << map.size() << "," << "B=" << map.bucket_count() << "," << "LF=" << map.load_factor() << "]" << "\n";
+}
 
 
   ///////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,        //
   // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP. //
   ///////////////////////////////////////////////
-
+for (auto p: map){                                  // iterates the pairs into the unordered map
+    cout << p.first << " " << p.second << "\n";     // Displays the pairs
+}
 
   cout << endl << "=== END func_unordered_map ===" << endl;
 }
